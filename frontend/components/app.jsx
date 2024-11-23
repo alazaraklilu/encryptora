7c53b9cce0b81da79c55e7e6ab72ffa55a4a756b
Alazar Aklilu <zar.aklilu@gmail.com>
2024-11-23 02:44:06 -0500

    login component built out

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import Login from './components/auth/login';
 import Register from './components/auth/register';
 
+const theme = createTheme({
+
+    palette: {
+        primary: {
+            main: "#1a73e8",
+        },
+        background: {
+            default: "#f4f6f8",
+        }
+    }
+});
+
+
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<h1>Welcome to the App</h1>} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Router>
+        <ThemeProvider>
+            <CssBaseline /> // reset default CSS and applies mui theme styling
+            <Router>
+                <Routes>
+                    <Route path="/" element={<h1>Welcome to the App</h1>} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
     );
 };
 
