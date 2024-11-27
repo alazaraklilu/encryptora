678e6085ad8119f18e36a580f445595eb6acfef1
Alazar Aklilu <zar.aklilu@gmail.com>
2024-11-26 22:32:03 -0500

    register layout

---
+++ frontend/components/auth/register.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+import { Box, Typography, TextField, Button } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
+
+function Register() {
+
+
+
+
+}
