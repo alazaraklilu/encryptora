fc0d73026ea246a4285a7894140c408764f39832
Alazar Aklilu <zar.aklilu@gmail.com>
2024-11-28 23:19:46 -0500

    registration mui component

--- frontend/components/auth/register.jsx
+++ frontend/components/auth/register.jsx
@@ -18,14 +18,54 @@ function Register() {
 
         }}
         >
+            <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12} sm={8} md={6}>
+                    <Box sx={{
+                        maxWidth: 400,
+                        width: "100%",
+                        bgcolor: "white",
+                        boxShadow: 3,
+                        borderRadius: 2,
+                        p: 4,
+                    }}
+                    >
+                        <Typography variant="h5" align="center" gutterBottom>Register for Encryptora</Typography>
 
-        </Box> 
-
-        <container></container>
-
-
+                        <Box component="form" sx={{ mt: 2 }}>
+                            <TextField
+                                label="Email"
+                                type="email"
+                                fullWidth
+                                required
+                                margin="normal"
+                            />
+                            <TextField
+                                label="Username"
+                                type="username"
+                                fullWidth
+                                required
+                                margin="normal"
+                            />
+                            <TextField
+                                label="Password"
+                                type="password"
+                                fullWidth
+                                required
+                                margin="normal"
+                            />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                                fullWidth
+                                sx={{ mt: 2 }}
+                            >Sign Up
+                            </Button>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
     )
-
-
 
 }
