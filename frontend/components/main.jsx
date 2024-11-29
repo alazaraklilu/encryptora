9692eaae4940de768891a2d8c8ac0298d7a52e86
Alazar Aklilu <zar.aklilu@gmail.com>
2024-11-28 23:51:53 -0500

    updated main

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app'; // Import your main app component
-import './styles.css'; // Optional CSS import for global styles
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
