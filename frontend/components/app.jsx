c587e2bd436fe80810231ee028a8201b5c81ad9b
Alazar Aklilu <zar.aklilu@gmail.com>
2024-12-10 23:08:50 -0500

    homepage component added, prework

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,32 +4,33 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Login from './auth/login';
 import Register from './auth/register';
+import Homepage from './homepage';
 
-const theme = createTheme({
+// const theme = createTheme({
 
-    palette: {
-        primary: {
-            main: "#1a73e8",
-        },
-        background: {
-            default: "#f4f6f8",
-        }
-    }
-});
+//     palette: {
+//         primary: {
+//             main: "#1a73e8",
+//         },
+//         background: {
+//             default: "#f4f6f8",
+//         }
+//     }
+// });
 
 
 const App = () => {
     return (
-        <ThemeProvider>
-            <CssBaseline />
-            <Router>
-                <Routes>
-                    <Route path="/" element={<h1>Welcome to the App</h1>} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Routes>
-            </Router>
-        </ThemeProvider>
+        // <ThemeProvider>
+        //     <CssBaseline />
+        <Router>
+            <Routes>
+                <Route path="/" element={<h1>Welcome to the App</h1>} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </Router>
+        // </ThemeProvider>
     );
 };
 
