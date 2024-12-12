e61db9edd128f152ba17ded10ff0e23eb1c30540
Alazar Aklilu <zar.aklilu@gmail.com>
2024-12-11 23:14:37 -0500

    homepage additions

--- frontend/components/homepage.jsx
+++ frontend/components/homepage.jsx
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from "react";
+import { Box, Typography, TextField, Button } from "@mui/material";
+import Grid from '@mui/material/Grid2';
 
 const Homepage = () => {
 
     return (
+        <Grid>
+            <Box sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: "100vh",
+                bgcolor: "background.default",
+                p: 3,
+            }}
+            >
 
-        
+            </Box>
+            <Typography>
+                <TextField>ENCRYPTORA</TextField>
+            </Typography>
+        </Grid>
 
     )
 
